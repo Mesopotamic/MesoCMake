43cf7f878ead99dcd5372298504f34423fb33698
Lawrence <Lawrence.martin.green@gmail.com>
2022-07-07 00:51:12 +0100

    Add a function that caches a string for platform dependant code paths

---
+++ Functions/meso_backend_selector.cmake
@@ -0,0 +1,23 @@
+# Detect which backend should be used from the current build params
+# from there we can automatically select the right file path
+# After the backend is selected return out, that way after 
+# reaching the end we can error out for unsupported backends
+
+# Defines
+# meso_gfx_backend - glsl, Wiigx etc
+# meso_window_backend - win32, wiigx etc
+
+
+if(WIN32)
+	set(meso_gfx_backend "glsl")
+	set(meso_window_backend "win32")
+	return()
+endif()
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "NintendoWii")
+	set(meso_gfx_backend "wii")
+	set(meso_window_backend "wii")
+	return()
+endif()
+
+message(FATAL_ERROR "Unknown or unsupported backend requested")
